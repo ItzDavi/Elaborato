1a4d4514dfb0b23fd9fae903e5a028278ea0f7f8
Davide <69810644+ItzDavi@users.noreply.github.com>
2021-05-08 16:52:24 +0200

    Added registration backend

--- Informatica/pages/register.php
+++ Informatica/pages/register.php
@@ -1,6 +1,37 @@
 <?php
+session_start();
 
-require_once("connection.php");
+//require_once("connection.php");
 
-echo $connection;
+$server = "localhost";
+$database = "gsh";
+$user = "root";
+$dbPassword = "";
+
+$connection = mysqli_connect($server, $user, $dbPassword, $database);
+
+
+if (!$connection) {
+  echo("Failed to connect to the database");
+  exit();
+} else {
+  $userName = $_POST['user-name'];
+  $userSurname = $_POST['user-surname'];
+  $userEmail = $_POST['user-email'];
+  $userPassword = $_POST['user-password'];
+
+  $checkEmail = "SELECT * FROM USERS WHERE email='$userEmail' LIMIT 1";
+  $checkResult = mysqli_query($connection, $checkEmail);
+  $user = mysqli_fetch_assoc($checkResult);
+
+  if ($user) {
+    echo("Email already registered");
+  } else {
+    $query = "INSERT INTO USERS (id_user, name, surname, email, user_password) VALUES (NULL, '$userName', '$userSurname', '$userEmail', '$userPassword')";
+    $insert = mysqli_query($connection, $query);
+
+    $_SESSION['email'] = $userEmail;
+  }
+}
+
 ?>
