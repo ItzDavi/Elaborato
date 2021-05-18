dc4de1feb1cb8535d68ee9bc9b2eca551feb3f93
Davide <69810644+ItzDavi@users.noreply.github.com>
2021-05-18 12:16:48 +0200

    Added initial php code to execute SQL

--- Informatica/pages/confirm.php
+++ Informatica/pages/confirm.php
@@ -1,6 +1,28 @@
 <?php
 
- ?>
+session_start();
+
+$server = "localhost";
+$database = "gsh";
+$user = "root";
+$dbpassword = "";
+
+$connection = mysqli_connect($server, $user, $dbpassword, $database);
+
+if (!$connection) {
+  echo("Failed to connect to the database");
+  exit();
+}
+
+$getDBInformation = "SELECT ";
+
+$insertOrder = "INSERT INTO orders VALUES (NULL, )";
+
+
+$planChecked = $_POST["plan"];
+
+print $planChecked;
+?>
 
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
