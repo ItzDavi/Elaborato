a4f03830eaf2692e8a68183c570e487c21b8efdc
Davide <69810644+ItzDavi@users.noreply.github.com>
2021-05-22 12:49:47 +0200

    Added code documentation for database connection

--- Informatica/pages/connection.php
+++ Informatica/pages/connection.php
@@ -1,12 +1,14 @@
 <?php
-
+//Database variables
 $server = "localhost";
 $database = "gsh";
 $user = "root";
 $dbpassword = "";
 
+//Database connection
 $connection = mysqli_connect($server, $user, $dbpassword, $database);
 
+//Database connection check
 if (!$connection) {
   echo("Failed to connect to the database");
   exit();
