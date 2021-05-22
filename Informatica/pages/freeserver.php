218a5c3dfb23a71ebc4a0d8f2b2f7b108577f437
Davide <69810644+ItzDavi@users.noreply.github.com>
2021-05-22 12:49:57 +0200

    Added freeserver.php code documentation

--- Informatica/pages/freeserver.php
+++ Informatica/pages/freeserver.php
@@ -1,13 +1,17 @@
 <?php
+//Start session
 session_start();
 
+//Database variables
 $server = "localhost";
 $database = "gsh";
 $user = "root";
 $dbpassword = "";
 
+//Database connection
 $connection = mysqli_connect($server, $user, $dbpassword, $database);
 
+//Database connection check
 if (!$connection) {
   echo("Failed to connect to the database");
   exit();
@@ -19,6 +23,7 @@ if (!$connection) {
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <!--CSS links-->
     <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="fontawesome/css/all.css">
